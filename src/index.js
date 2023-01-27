6e5eab2e4ef8d196ccc1e4ef71315526a776a4fd
Katsiarynna <113954361+Katsiarynna@users.noreply.github.com>
2023-01-27 14:45:43 +0100

    fix: towel

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+    var matrix = [
+        [1, 2, 3,],
+        [4, 5, 6,],
+        [7, 8, 9]
+    ];
   return [];
 }
